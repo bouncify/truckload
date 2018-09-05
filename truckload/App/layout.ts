f39656c46e4b4f81e2baa6f511a62a6b5fe1d60d
James Rounce <james.rounce@univar.com>
2018-09-05 14:00:14 -0700

    added site image

--- truckload/App/layout.ts
+++ truckload/App/layout.ts
@@ -5,5 +5,18 @@ import "jquery-validation"
 
 var baseUrl = window.location.origin + $("#SiteDirectory").val();
 
+import "kendo-ui-core"
+
+
+$("#dropDownList").kendoDropDownList({
+    dataTextField: "text",
+    dataValueField: "value",
+    dataSource: [
+        { text: "Item1", value: "1" },
+        { text: "Item2", value: "2" }
+    ]
+});
+
+
 //alert(baseUrl);
 
--- truckload/App_Js/VisualStudio-AutoCompile/layout.js
+++ truckload/App_Js/VisualStudio-AutoCompile/layout.js
@@ -5,5 +5,14 @@ require("./css/Site.less");
 require("bootstrap");
 require("jquery-validation");
 var baseUrl = window.location.origin + $("#SiteDirectory").val();
+require("kendo-ui-core");
+$("#dropDownList").kendoDropDownList({
+    dataTextField: "text",
+    dataValueField: "value",
+    dataSource: [
+        { text: "Item1", value: "1" },
+        { text: "Item2", value: "2" }
+    ]
+});
 //alert(baseUrl);
 //# sourceMappingURL=layout.js.map
