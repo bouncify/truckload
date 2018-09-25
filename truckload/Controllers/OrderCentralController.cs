7bdb30f3177408b76dd352196807929c419424f6
James Rounce <james.rounce@outlook.com>
2018-09-24 21:19:53 -0700

    fixed menu size for order central

---
+++ truckload/Controllers/OrderCentralController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace truckload.Controllers
+{
+    [Authorize]
+    public class OrderCentralController : BaseController
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            InitViewData(TabMenu.OrderCentral);
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.IsCustomContainer = true;
+            return View();
+        }
+    }
+}
