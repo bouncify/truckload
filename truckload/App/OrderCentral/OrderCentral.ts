d34f1632fb1eeaab45bd0311afd4ae9884bb29a3
James Rounce <james.rounce@outlook.com>
2018-09-27 20:20:37 -0700

    clean up

--- truckload/App/OrderCentral/OrderCentral.ts
+++ truckload/App/OrderCentral/OrderCentral.ts
@@ -2,24 +2,15 @@
 import "kendo/js/kendo.grid.js"
 import "kendo/js/kendo.dialog.js"
 import "kendo/js/kendo.aspnetmvc.js"
-import { Globals } from "../Shared/Global"
-//import * as ko from 'knockout';
 import "knockout-mapping"
 import 'knockout-kendo/build/knockout-kendo';
 
 import { Main } from "./Classes/Main"
-import { Startup } from "./Modules/Startup"
 
-
-
-//$("#containerBody").css("width",Globals.bodyWidth);
 
 export var module: Main;
 
-export function initMainModule() { module = Startup.init()}
+export function initMainModule() { module = new Main()}
 
 
-//export var module: CrudScreenAutoSave;
-
-//export function initDriverModule() { module = Driver.init(); }
 
