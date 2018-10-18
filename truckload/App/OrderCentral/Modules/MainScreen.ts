ca96756b85a5321577e585a0f2ef17cf1f3fe726
James Rounce <james.rounce@outlook.com>
2018-10-17 21:56:34 -0700

    added load col details

--- truckload/App/OrderCentral/Modules/MainScreen.ts
+++ truckload/App/OrderCentral/Modules/MainScreen.ts
@@ -11,6 +11,14 @@ export module MainScreen {
 
         $("#containerBody").width(screenWidth);
 
+
+        //var calendarButton = $("#miniLoadDateButton")[0].offsetParent.children[1].childNodes[0] as HTMLSpanElement;
+
+        //calendarButton.style.position = "absolute";
+        //calendarButton.style.marginTop = "2px";
+        //calendarButton.style.marginLeft = "0";
+        //calendarButton.style.left = "7px";
+
         //$("#loadCol1").height(sharedModel.gridHeight);
 
         for (let i = 1; i < totalLoadCols ; i++) {
@@ -18,7 +26,7 @@ export module MainScreen {
 
             var newCol = $("#loadCol1").clone();
             newCol.attr("id", `loadCol${colNo}`);
-            newCol.text("Load Col" + colNo);
+            //newCol.text("Load Col" + colNo);
 
             newCol.insertAfter("div.loadCol:last");
 
