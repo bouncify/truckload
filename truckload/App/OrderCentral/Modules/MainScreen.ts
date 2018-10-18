32d26c4cbe5ac16ee32564f2cc3127b7c72ccd83
James Rounce <james.rounce@outlook.com>
2018-10-17 19:52:40 -0700

    create load columns

--- truckload/App/OrderCentral/Modules/MainScreen.ts
+++ truckload/App/OrderCentral/Modules/MainScreen.ts
@@ -5,14 +5,31 @@
 export module MainScreen {
     export function init(sharedModel: SharedModel) {
 
-        var screenWidth = sharedModel.loadColWidth * (sharedModel.visibleLoadCols + 1);
+        var totalLoadCols = sharedModel.visibleLoadCols;
+
+        var screenWidth = sharedModel.loadColWidth * (totalLoadCols + 1);
 
         $("#containerBody").width(screenWidth);
+
+        for (let i = 0; i < totalLoadCols -1; i++) {
+            //$("name='loadCol'").clone().insertAfter("loadCol:last");
+
+            $("#loadCol1").clone().insertAfter("div.loadCol:last");
+            //console.log(i);
+        }
 
         //sharedModel.visibleLoads
         //sharedModel.bodyWidth
         //sharedModel.gridArray
 
+        //var amount = 5;
+        //for (var i = 0; i < amount; i++) {
+        //    var newDiv = document.createElement("div");
+        //    newDiv.innerText = "wibble";
+        //    //newDiv.className = "bluestrip";
+        //    document.body.appendChild(newDiv);
+        //    //console.log("This is repeat " + i);
+        //}
 
         console.log("finished main screen setup");
     }
